6b56c8f56c37bca6279d3557f74a73b428e1739a
johnmarsden24 <john.marsden24@gmail.com>
2020-09-14 17:14:00 +0100

    refactored all animation and cleaned up

--- public/js/index.js
+++ public/js/index.js
@@ -2,57 +2,23 @@ import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import {
   createScrollAnimationMultiple,
-  newScrollAnimationFlowBotherText,
-  createScrollFadeInAnimationTimeline,
+  createFadeInAnimation,
   lottieLoader,
-  toggleActive,
-  parallaxAnimation,
 } from "./basePage";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const animatedCards = document.querySelectorAll(".animated-card");
-const parallaxCards = document.querySelectorAll(".parallax-card");
-const newFlowBotherText = document.querySelectorAll(".new-flow-bother-text");
-const waveTitles = document.querySelector(".wave-container__titles");
+const parallaxCards = document.querySelectorAll(".parallax");
+const waveCards = document.querySelectorAll(".wave-card");
 
 const animateOpts = { loadLottie: true, parallax: false };
 const parallaxOpts = { loadLottie: false, parallax: true };
+const waveOpts = { loadLottie: false, parallax: false };
 
 createScrollAnimationMultiple(animatedCards, animateOpts);
 createScrollAnimationMultiple(parallaxCards, parallaxOpts);
-newScrollAnimationFlowBotherText(newFlowBotherText);
-createScrollFadeInAnimationTimeline();
+createScrollAnimationMultiple(waveCards, waveOpts);
+
+createFadeInAnimation();
 lottieLoader();
-// parallaxAnimation();
-
-waveTitles.addEventListener("click", toggleActive);
-
-// (function (doc, win) {
-//   var docEl = doc.documentElement,
-//     recalc = function () {
-//       var clientWidth = docEl.clientWidth;
-//       if (!clientWidth) return;
-//       if (clientWidth < 1000) {
-//         return (docEl.style.fontSize = "1rem");
-//       }
-//       docEl.style.fontSize = clientWidth / 1440 + "rem";
-//       docEl.style.fontSize;
-//       docEl.style.display = "none";
-//       docEl.clientWidth; // Force relayout - important to new Androids
-//       docEl.style.display = "";
-//     };
-
-//   // Abort if browser does not support addEventListener
-//   if (!doc.addEventListener) return;
-
-//   // Test rem support
-//   var div = doc.createElement("div");
-//   div.setAttribute("style", "font-size: 1rem");
-
-//   // Abort if browser does not recognize rem
-//   if (div.style.fontSize != "1rem") return;
-
-//   win.addEventListener("resize", recalc, false);
-//   doc.addEventListener("DOMContentLoaded", recalc, false);
-// })(document, window);
